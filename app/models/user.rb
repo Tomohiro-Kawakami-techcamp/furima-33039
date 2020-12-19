c8a7b8355b194f4f87ef0bcb4c0be79dc5d13f78
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-19 18:26:39 +0900

    登録画面

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :birth, presence: true
+  # validates :birth, presence: true
 end
