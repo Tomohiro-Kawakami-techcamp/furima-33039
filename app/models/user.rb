01a8b47d5e42627df61b089288395d1f6284bb4a
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-20 20:16:29 +0900

    不要な箇所、with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,13 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  end
   validates :birth, presence: true
 end
