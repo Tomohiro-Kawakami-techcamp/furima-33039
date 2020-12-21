7d83fa09255cefe18e6a01e9405b30c28890d489
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-21 22:34:38 +0900

    エラー文

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,13 +12,14 @@ class Item < ApplicationRecord
   validates :image,                   presence: true
   validates :name,                    presence: true,  unless: :was_attached?
   validates :info,                    presence: true,  unless: :was_attached?
-  validates :price,                   presence: true,  unless: :was_attached?
-  validates :category_id,             numericality: { other_than: 1 }, unless: :was_attached? 
-  validates :sales_status_id,         numericality: { other_than: 1 }, unless: :was_attached? 
-  validates :shipping_fee_status_id,  numericality: { other_than: 1 }, unless: :was_attached? 
-  validates :prefecture_id,           numericality: { other_than: 1 }, unless: :was_attached? 
-  validates :scheduled_delivery_id,   numericality: { other_than: 1 }, unless: :was_attached? 
-  
+  validates :price,                   presence: true,  numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }, unless: :was_attached?
+  validates :price,                   presence: true,  format: { with: /\d[300-9999999]\d/, message: 'Out of setting range' }, unless: :was_attached?
+  validates :category_id,             numericality: { other_than: 0, message: 'Select'  }, unless: :was_attached? 
+  validates :sales_status_id,         numericality: { other_than: 0, message: 'Select' }, unless: :was_attached? 
+  validates :shipping_fee_status_id,  numericality: { other_than: 0, message: 'Select' }, unless: :was_attached? 
+  validates :prefecture_id,           numericality: { other_than: 0, message: 'Select' }, unless: :was_attached? 
+  validates :scheduled_delivery_id,   numericality: { other_than: 0, message: 'Select' }, unless: :was_attached? 
+
   def was_attached?
     self.image.attached?
   end
