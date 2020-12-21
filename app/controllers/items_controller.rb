6c595824a08ada289a188424c54550ed51b2d61e
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-21 11:28:44 +0900

    ルーティング直し、コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,10 @@ class ItemsController < ApplicationController
    @items = Item.all
   end
 
+  def new
+    @item = Item.new
+  end
+
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
