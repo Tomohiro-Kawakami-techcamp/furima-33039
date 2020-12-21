96e1fdc0d9956c6de0d365ca25e27e5f1ab59c4d
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-21 11:20:20 +0900

    バリデーション、newを加える

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image,                   presence: true
+  validates :name,                    presence: true,  unless: :was_attached?
+  validates :info,                    presence: true,  unless: :was_attached?
+  validates :price,                   presence: true,  unless: :was_attached?
+  validates :category_id,             presence: true,  unless: :was_attached?
+  validates :sales_status_id,         presence: true,  unless: :was_attached?
+  validates :shipping_fee_status_id,  presence: true,  unless: :was_attached?
+  validates :prefecture_id,           presence: true,  unless: :was_attached?
+  validates :scheduled_delivery_id,   presence: true,  unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
