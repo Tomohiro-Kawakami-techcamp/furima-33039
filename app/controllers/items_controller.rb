ab9deb6abe2ee4b43926d879f1981fb7335aa144
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-21 19:11:10 +0900

    プルダウンと自動計算

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,15 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to  items_path
+    else
+      render :new
+    end
+  end
+
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,22 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
   
   validates :image,                   presence: true
   validates :name,                    presence: true,  unless: :was_attached?
   validates :info,                    presence: true,  unless: :was_attached?
   validates :price,                   presence: true,  unless: :was_attached?
-  validates :category_id,             presence: true,  unless: :was_attached?
-  validates :sales_status_id,         presence: true,  unless: :was_attached?
-  validates :shipping_fee_status_id,  presence: true,  unless: :was_attached?
+  validates :category_id,             numericality: { other_than: 1 }, unless: :was_attached? 
+  validates :sales_status_id,         numericality: { other_than: 1 }, unless: :was_attached? 
+  validates :shipping_fee_status_id,  numericality: { other_than: 1 }, unless: :was_attached? 
   validates :prefecture_id,           numericality: { other_than: 1 }, unless: :was_attached? 
-  validates :scheduled_delivery_id,   presence: true,  unless: :was_attached?
-
+  validates :scheduled_delivery_id,   numericality: { other_than: 1 }, unless: :was_attached? 
+  
   def was_attached?
     self.image.attached?
   end
