1ac4f6b296e565afcc088aca9bf945133ad294bf
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-22 18:59:34 +0900

    with_options

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
   
-  validates :image,                   presence: true
-  validates :name,                    presence: true
-  validates :info,                    presence: true
-  validates :price,                   presence: true,  numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  #validates :price,                   presence: true,  format: { with: /\d[300-9999999]\d/, message: 'Out of setting range' }
+  with_options presence: true do
+  validates :image
+  validates :name
+  validates :info
+  validates :price,    numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  end
+  
   validates_inclusion_of :price, in:300..9999999, message: 'Out of setting range'
-  validates :category_id,             numericality: { other_than: 0, message: 'Select'  }
-  validates :sales_status_id,         numericality: { other_than: 0, message: 'Select' } 
-  validates :shipping_fee_status_id,  numericality: { other_than: 0, message: 'Select' } 
-  validates :prefecture_id,           numericality: { other_than: 0, message: 'Select' } 
-  validates :scheduled_delivery_id,   numericality: { other_than: 0, message: 'Select' } 
-
+  
+  with_options numericality: { other_than: 0, message: 'Select'  } do
+  validates :category_id
+  validates :sales_status_id
+  validates :shipping_fee_status_id 
+  validates :prefecture_id 
+  validates :scheduled_delivery_id 
+  end
 end
