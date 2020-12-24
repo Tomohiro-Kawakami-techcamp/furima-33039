56c1f8541c0ed77a15723e258dbc734df1cf929c
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-24 15:43:03 +0900

    変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show]
+  before_action :move_to_index, only: :edit
 
   def index
    @items = Item.all.order("created_at DESC")
@@ -20,6 +21,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -41,6 +43,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
