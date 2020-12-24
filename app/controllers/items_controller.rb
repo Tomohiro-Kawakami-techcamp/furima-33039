31a3e338f5c9bcb8ccc9de739cddaaef5a584d13
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2020-12-24 11:17:56 +0900

    エラーメッセージのためのモデル、売却済みの条件を加える

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  except: [:index, :show]
+  before_action :move_to_index, only: :edit
 
   def index
    @items = Item.all.order("created_at DESC")
@@ -27,16 +28,25 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
       redirect_to  item_path
     else
       render :edit
     end
   end
 
+  private
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
