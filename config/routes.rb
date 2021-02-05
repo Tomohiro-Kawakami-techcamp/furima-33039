1db8d41b4390fdefebe3347eeef5cca9a65ebdf3
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-05 11:37:33 +0900

    変更とここまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  get 'products/search'
+  get 'items/search'
   resources :items do
     resources :orders, only: [:index, :create]
   end
